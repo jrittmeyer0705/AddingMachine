97b1af4940ac25ec4b4ad15e6409e022b7d67e9c
Justin <Justin@DESKTOP-I420F0T>
2020-10-06 22:28:50 -0700

    JavaDoc update

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -2,25 +2,43 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
-  
+  /**
+   * AddingMachine constructor takes in no parameters  
+   * returns nothing
+   */
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
-  
+ /**
+  * gettotal takes in no parameters  
+  * returns 0
+  */
   public int getTotal () {
     return 0;
   }
-  
+  /**
+   * Add takes in int value  
+   * returns nothing
+   */
   public void add (int value) {
   }
-
+  /**
+   * subtract takes in int value 
+   * returns nothing
+   */
   public void subtract (int value) {
   }
-
+  /**
+   * ToString takes in no parameters  
+   * returns nothing as of right now
+   */
   public String toString () {
     return "";
   }
-
+  /**
+   * clear takes in no parameters  
+   * returns nothing
+   */
   public void clear() {
   }
 }
