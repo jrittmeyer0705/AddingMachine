95aa0a82c87dd34a7bdb4a1d2da042d5d460cfde
Justin <Justin@DESKTOP-I420F0T>
2020-10-07 21:01:03 -0700

    Javadoc Comments

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -1,64 +1,81 @@
 package cse360assignment02; 
- 
+/**
+* The AddingMachine keeps track of calculations performed by the myCalculator object in this program.
+*
+* @author  Justin Rittmeyer
+* @version 1.0
+* @since   2020-10-7 
+*/
 public class AddingMachine {  
 private int total;
 private String history;
 /**
- * AddingMachine constructor takes in no parameters  
- * returns nothing
- */
+* This method has contents in it that just enhance clarity in the program
+* total=0 and history = "0" for clarity
+* @param Unused
+* @return Nothing
+*/
 public AddingMachine () {
 	
 	total = 0; // not needed - included for clarity  
 	history="0";
 }     
 /**
- * getTotal takes in no parameters  
- * returns total
- */
+* This method has the total that is used throughout the program.
+* @param Unused
+* @return int getTotal
+*/
  public int getTotal () {
 	 
 	 return total;   
  }    
  /**
-  * add takes in int value as a parameter  
-  * returns nothing but adds the value to toString
-  */
+ * This method adds the total to the value. Also adds the integer value in the history string which gets printed out by main in this program.
+ * @param int value, this is the only parameter in this method.
+ * @return Nothing
+ */
  public void add (int value) {
 	 
 	 total=total+value;
 	 history=history+" + "+Integer.toString(value);
  } 
  /**
-  * subtract takes in an int value as parameter  
-  * returns nothing but subtracts the value and adds it to toString
-  */
+ * This method subtracts total from value. Also subtracts the integer value in the history string which gets printed out by main in this program.
+ * @param int value, this is the only parameter in this method
+ * @return Nothing
+ */
  public void subtract (int value) {
 	 
 	 total=total-value;
 	 history=history+" - "+Integer.toString(value);
  } 
  /**
-  * toString method takes in no parameters 
-  * returns history
-  */
+ * This method contains the toString method to represent any object as a string
+ * @param Unused
+ * @return String that contains history
+ */
   public String toString ()  {
 	  
 	  return history;   
   } 
   /**
-   * clear method takes in no parameters  
-   * 
-   */
+  * Clear method, has no effect on programs output
+  * @param Unused
+  * @return Nothing
+  */
   public void clear()  {
 	  
 	  history="";
   }
   
   /**
-   * Main method takes in string[] args  
-   * Initializes AddingMachine and performs all calculations prints out myCalculator.toString
-   */ 
+   * This is the main method which makes use of add, subtract and toString methods.
+   * the system prints out the contents of the toString method which contains the history of all the calculations
+   * done by the myCalculator object
+   * myCalculator gets created using the new keyword in main
+   * @param args Unused.
+   * @return Nothing.
+   */
   public static void main(String[] args) {
 	  
 	  AddingMachine myCalculator =new AddingMachine();
